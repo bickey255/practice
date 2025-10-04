ac9574335979ddb1476c6679a390bdfc7709d9e4
Bickey yadav <128046543+bickey255@users.noreply.github.com>
2025-10-04 05:06:36 -0700

    main

--- server/index.js
+++ server/index.js
@@ -17,3 +17,31 @@ const acc = document.getElementsByClassName('containte');
 for(let i=0; i<acc.length; i++){
     acc[i].style.background="yellow";
 }
+
+//5---create new button and also proform some task like change background, font color,alert,prompt,and using loop
+// print number untill 15 using for loop /while loop everything should be done with javascript
+
+const newbtn = document.createElement('button');
+newbtn.setAttribute("class","help");
+newbtn.innerText="click me!!";
+newbtn.style.background="red";
+newbtn.style.color="white";
+console.log(newbtn);
+// dont write semi-colean in append
+const pprint = document.body.appendChild(newbtn)
+const cons = document.getElementsByClassName("help");
+cons[0].onclick =()=>{
+    alert("This might cause danger");
+    let i = prompt("Enter the number");
+    if(i%2==0){
+        console.log("This is Even number");
+    }else
+    {
+        console.log("Thsi is odd number");
+
+    }
+    for(let i=0; i<=20; i++){
+        console.log(i);
+    }
+    console.log("Keep on trying");
+}
