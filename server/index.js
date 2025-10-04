c348dc3a81173a814997c38b6586dc80186ae31d
Bickey yadav <128046543+bickey255@users.noreply.github.com>
2025-10-04 04:29:35 -0700

    changed

--- server/index.js
+++ server/index.js
@@ -3,3 +3,17 @@
 const  sum = (a,b) =>{
     return a+b;
 }
+
+//accesing the class called containet will add some css design using javascript
+
+const acc = document.getElementsByClassName('containte');
+//to change the first element of the class we need ude access bracket and index point 
+//to change all class design we need to use the loop
+// acc[0].style.background="red";
+// console.log(acc);
+
+
+//we will use for loop to change all class backgroung at once :
+for(let i=0; i<acc.length; i++){
+    acc[i].style.background="yellow";
+}
